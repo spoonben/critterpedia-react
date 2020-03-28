c96b32b678f48bf0da5977f5b4919b6ffae632a4
Ben Spoon <ben.spoon@autodesk.com>
2020-03-28 16:17:47 -0700

    basic

--- server.js
+++ server.js
@@ -6,10 +6,12 @@ const express = require('express');
 const app = express();
 const path = require('path');
 
+app.use(express.static(__dirname + '/dist'));
+
 // viewed at http://localhost:8080
 app.get('/', function(req, res) {
   res.sendFile(path.join(__dirname + '/dist/index.html'));
 });
 
-console.log(`\n \n \nListening on  ${port}`)
+console.log(`\n \n \nListening on ${port}`)
 app.listen(8080);
