72c435471df74c0b228ede3a2eb8257160b28c9e
Ben Spoon <ben.spoon@autodesk.com>
2020-03-28 16:23:16 -0700

    finally

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,6 @@
 const path = require('path');
-var HtmlWebpackPlugin = require('html-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CopyPlugin = require('copy-webpack-plugin');
 
 module.exports = {
   entry: './src/index.jsx',
@@ -22,6 +23,9 @@ module.exports = {
   plugins: [
       new HtmlWebpackPlugin({
         template: 'src/index.html'
-      })
+      }),
+      new CopyPlugin([
+        { from: 'src/static' },
+      ]),
     ]
 };
