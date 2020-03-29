67f920166926e3aefb225d5ed08ffbb2f4b5b7cc
Thomas Beirne <thomas@buildingconnected.com>
2020-03-29 13:20:53 -0700

    Add logic for parsing month strings

--- src/parse-months.js
+++ src/parse-months.js
@@ -1,3 +1,55 @@
+export const range = (start, end, cycleSize) => {
+  if (end < start) return range(start, cycleSize + end, cycleSize);
+
+  return new Array(end - start + 1)
+    .fill()
+    .map((_, i) => ((start + i - 1) % cycleSize) + 1);
+};
+
+const monthToNum = (month) => {
+  switch (month) {
+    case "January":
+      return 1;
+    case "February":
+      return 2;
+    case "March":
+      return 3;
+    case "April":
+      return 4;
+    case "May":
+      return 5;
+    case "June":
+      return 6;
+    case "July":
+      return 7;
+    case "August":
+      return 8;
+    case "September":
+      return 9;
+    case "October":
+      return 10;
+    case "November":
+      return 11;
+    case "December":
+      return 12;
+  }
+};
+
+const parseRegExp = /(\w+)-(\w+) \(Northern\) \/ (\w+)-(\w+) \(Southern\)/;
 export const parseMonths = (monthStr) => {
-  return [];
+  if (monthStr === "Year-round (Northern and Southern)") {
+    return {
+      northern: range(1, 12, 12),
+      southern: range(1, 12, 12),
+    };
+  }
+  const [startNorth, endNorth, startSouth, endSouth] = parseRegExp
+    .exec(monthStr)
+    .slice(1)
+    .map(monthToNum);
+
+  return {
+    northern: range(startNorth, endNorth, 12),
+    southern: range(startSouth, endSouth, 12),
+  };
 };
--- src/parse-months.test.js
+++ src/parse-months.test.js
@@ -1,9 +1,25 @@
-import { parseMonths } from "./parse-months";
+import { range, parseMonths } from "./parse-months";
 
-test("It works", () => {
+test("Simple range", () => {
+  expect(range(1, 3, 5)).toEqual([1, 2, 3]);
+});
+
+test("Looped range", () => {
+  console.log("LOOPED RANGE", range(4, 1, 5));
+  expect(range(4, 1, 5)).toEqual([4, 5, 1]);
+});
+
+test("Parse months that differ across hemispheres", () => {
+  expect(parseMonths("Year-round (Northern and Southern)")).toEqual({
+    northern: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12],
+    southern: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12],
+  });
+});
+
+test("Parse months that differ across hemispheres", () => {
   expect(
     parseMonths("September-June (Northern) / March-December (Southern)")
-  ).toBe({
+  ).toEqual({
     northern: [9, 10, 11, 12, 1, 2, 3, 4, 5, 6],
     southern: [3, 4, 5, 6, 7, 8, 9, 10, 11, 12],
   });
