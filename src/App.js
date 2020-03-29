6ee53dd47e27f1a2e05de7df87c80bfb3201af2f
Ben Spoon <ben.spoon@autodesk.com>
2020-03-28 23:02:08 -0700

    move results into its own thing

--- src/App.js
+++ src/App.js
@@ -2,8 +2,8 @@ import React, { useState, useEffect } from "react";
 
 import { fullList } from "./lists";
 
-import Calendar from "./components/calendar";
 import HemisphereSelect from "./components/hemisphere-select";
+import Results from "./components/results";
 
 const App = () => {
   const [searchText, setSearchText] = useState("");
@@ -27,41 +27,6 @@ const App = () => {
     setHemisphere(value);
   };
 
-  const ResultsDiv = ({ results }) => (
-    <div>
-      {results.map(result => {
-        return (
-          <div className="critterCard" key={result.name + result.critterNumber}>
-            <div className="number">#{result.critterNumber}</div>
-            <div className="name">{result.name}</div>
-            {result.shadowSize && <div>Shadow Size{result.shadowSize}</div>}
-            <div>Value: {result.value}</div>
-            <div className="availablitly">
-              <div>
-                {result.available && (
-                  <span className="card-title">Seasonability</span>
-                )}
-                {result.available && (
-                  <Calendar availablitly={result.available[hemisphere]} />
-                )}
-              </div>
-              <div className="availablitly-spacer" />
-              <div>
-                <span className="card-title">Current Active Hours:</span>{" "}
-                <div>{result.time}</div>
-              </div>
-            </div>
-            <div className="location-wrap">
-              <div>
-                <span className="card-title">Location:</span> {result.location}
-              </div>
-            </div>
-          </div>
-        );
-      })}
-    </div>
-  );
-
   return (
     <div className="main">
       <h1>Animal Crossing Critter Search</h1>
@@ -75,7 +40,7 @@ const App = () => {
         selectedOption={hemisphere}
         handleChange={changeHemisphere}
       />
-      <ResultsDiv results={resultsList} />
+      <Results results={resultsList} hemisphere={hemisphere} />
     </div>
   );
 };
