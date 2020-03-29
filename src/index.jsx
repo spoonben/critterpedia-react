f27ebc4e37bf2ff77be4d057420453c47740f52c
Thomas Beirne <thomas@buildingconnected.com>
2020-03-28 18:33:50 -0700

    Set up Prettier

--- src/index.jsx
+++ src/index.jsx
@@ -1,8 +1,8 @@
 // Application entrypoint.
 
 // Render the top-level React component
-import React from 'react';
-import ReactDom from 'react-dom'
-import App from './App.jsx';
+import React from "react";
+import ReactDom from "react-dom";
+import App from "./App.jsx";
 
-ReactDom.render(<App />, document.getElementById('react-root'));
+ReactDom.render(<App />, document.getElementById("react-root"));
--- webpack.config.js
+++ webpack.config.js
@@ -1,27 +1,23 @@
-const path = require('path');
-const CopyPlugin = require('copy-webpack-plugin');
+const path = require('path')
+const CopyPlugin = require('copy-webpack-plugin')
 
 module.exports = {
-  entry: './src/index.jsx',
-  output: {
-    filename: 'bundle.js',
-    path: path.resolve(__dirname, 'dist'),
-  },
-  mode: 'production',
-  module: {
-      rules: [
-        {
-          test: /\.(js|jsx)$/,
-          exclude: /node_modules/,
-          use: {
-            loader: "babel-loader"
-          }
-        }
-      ]
-    },
-  plugins: [
-      new CopyPlugin([
-        { from: 'src/static' },
-      ]),
-    ]
-};
+	entry: './src/index.jsx',
+	output: {
+		filename: 'bundle.js',
+		path: path.resolve(__dirname, 'dist'),
+	},
+	mode: 'production',
+	module: {
+		rules: [
+			{
+				test: /\.(js|jsx)$/,
+				exclude: /node_modules/,
+				use: {
+					loader: 'babel-loader',
+				},
+			},
+		],
+	},
+	plugins: [new CopyPlugin([{ from: 'src/static' }])],
+}
