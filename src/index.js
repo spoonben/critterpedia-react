99f9dc835057baa554fe6c44d2573e9eac2dd6ce
Ben Spoon <ben.spoon@autodesk.com>
2020-03-28 20:11:10 -0700

    Update webpack and app

--- src/index.js
+++ src/index.js
@@ -3,6 +3,6 @@
 // Render the top-level React component
 import React from "react";
 import ReactDom from "react-dom";
-import App from "./App.jsx";
+import App from "./App.js";
 
 ReactDom.render(<App />, document.getElementById("react-root"));
--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,7 @@ const path = require('path')
 const CopyPlugin = require('copy-webpack-plugin')
 
 module.exports = {
-	entry: './src/index.jsx',
+	entry: './src/index.js',
 	output: {
 		filename: 'bundle.js',
 		path: path.resolve(__dirname, 'dist'),
