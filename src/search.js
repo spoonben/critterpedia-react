ff1dbc0efcb834a187ff4eec7526d27f27bb30ef
Ben Spoon <ben@benspoon.com>
2020-04-02 12:15:05 -0700

    prettier

--- src/search.js
+++ src/search.js
@@ -3,62 +3,62 @@ import * as R from "ramda";
 import { bugs, fish } from "./lists.json";
 
 const critterTypes = {
-	fish,
-	bugs,
-	both: fish.concat(bugs),
+  fish,
+  bugs,
+  both: fish.concat(bugs),
 };
 
 const sortBy = (filter) => R.sortBy(R.prop(filter));
 
 const filterEverything = ({
-	monthsToFilter,
-	hemisphere,
-	critterToSearch,
-	searchText,
-	leavingNow,
+  monthsToFilter,
+  hemisphere,
+  critterToSearch,
+  searchText,
+  leavingNow,
 }) => {
-	let crittersFiltered = critterToSearch;
-	if (searchText) {
-		crittersFiltered = crittersFiltered.filter((item) =>
-			item.name.toLowerCase().includes(searchText.toLowerCase())
-		);
-	}
-	if (leavingNow) {
-		const currentMonth = new Date().getMonth() + 1; // because javascript thinks jan is 0
-		crittersFiltered = crittersFiltered.filter(
-			(item) =>
-				item.available[hemisphere].includes(currentMonth) &&
-				!item.available[hemisphere].includes(currentMonth + 1)
-		);
-	}
-	if (!leavingNow && monthsToFilter && monthsToFilter.length > 0) {
-		crittersFiltered = crittersFiltered.filter((item) => {
-			const isAvailable = (month) => item.available[hemisphere].includes(month);
-			return R.all(isAvailable)(monthsToFilter);
-		});
-	}
-	return crittersFiltered;
+  let crittersFiltered = critterToSearch;
+  if (searchText) {
+    crittersFiltered = crittersFiltered.filter((item) =>
+      item.name.toLowerCase().includes(searchText.toLowerCase())
+    );
+  }
+  if (leavingNow) {
+    const currentMonth = new Date().getMonth() + 1; // because javascript thinks jan is 0
+    crittersFiltered = crittersFiltered.filter(
+      (item) =>
+        item.available[hemisphere].includes(currentMonth) &&
+        !item.available[hemisphere].includes(currentMonth + 1)
+    );
+  }
+  if (!leavingNow && monthsToFilter && monthsToFilter.length > 0) {
+    crittersFiltered = crittersFiltered.filter((item) => {
+      const isAvailable = (month) => item.available[hemisphere].includes(month);
+      return R.all(isAvailable)(monthsToFilter);
+    });
+  }
+  return crittersFiltered;
 };
 
 const search = ({
-	searchText,
-	leavingNow,
-	hemisphere,
-	monthsToFilter,
-	sort = "name",
-	critterType = "both",
+  searchText,
+  leavingNow,
+  hemisphere,
+  monthsToFilter,
+  sort = "name",
+  critterType = "both",
 }) => {
-	const critterToSearch =
-		critterType === "both" ? critterTypes.both : critterTypes[critterType];
-	const resultsToReturn = filterEverything({
-		monthsToFilter,
-		hemisphere,
-		critterToSearch,
-		searchText,
-		leavingNow,
-	});
-	const sortedResults = sortBy(sort)(resultsToReturn);
-	return sort === "value" ? R.reverse(sortedResults) : sortedResults;
+  const critterToSearch =
+    critterType === "both" ? critterTypes.both : critterTypes[critterType];
+  const resultsToReturn = filterEverything({
+    monthsToFilter,
+    hemisphere,
+    critterToSearch,
+    searchText,
+    leavingNow,
+  });
+  const sortedResults = sortBy(sort)(resultsToReturn);
+  return sort === "value" ? R.reverse(sortedResults) : sortedResults;
 };
 
 export default search;
